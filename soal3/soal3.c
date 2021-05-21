2a88e58f1e9988a0201b9852175e676fb0fdb493
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2021-05-21 22:03:38 +0700

    updated no 3

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,98 +1,158 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <syslog.h>
-#include <string.h>
-#include <math.h>
-#include <time.h>
-#include <dirent.h>
-#include <string.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <pthread.h>
-#include <ctype.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<dirent.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<sys/wait.h>
 
-char cwd[100];
-pthread_t tid[3];
+pthread_t tid[1000000];
 pid_t child;
-char *checkext[4],*checkext2[20],namafile[100],dummyinput[100];
-int n = 0, m = 0;
+char cwd[1000];
+char folder[1000];
+char deepfolder[1000];
+char deepfolder2[1000];
+int err;
 
-void* movinfile(void *arg){
-    strcpy(dummyinput,arg);
-    char *token,*token1;
+void *move(void *arg) {
+	char *filepath = (char *)arg;
+	char *extension = NULL;
+	extension = strchr(filepath, '.');
+	char ext[1000];
+	memset(ext, '\0', sizeof(ext));
+	if((extension-filepath-strlen(cwd)+1)==2 || (extension-filepath+1)==1) strcpy(ext, "Hidden");
+	else if(extension) {
+		extension++;
+        for(int i=0; i<strlen(extension); i++) {
+               	ext[i] = tolower(extension[i]);
+    	}
+	}
+	else strcpy(ext, "Unknown");
+    char *filename = NULL;
+    filename = strrchr(filepath, '/');
+    if(filename) filename++;
+	else filename = filepath;
 
-	unsigned long i=0;
-	pthread_t id = pthread_self();
-
-    token1 = strtok(dummyinput, "/");
-    while( token1 != NULL ) {
-        checkext2[m] = token1;
-        m++;
-        token1 = strtok(NULL, "/");
-    }
-    strcpy(namafile,checkext2[m-1]);
-
-    token = strtok(checkext2[m-1], ".");
-    while( token != NULL ) {
-        checkext[n] = token;
-        n++;
-        token = strtok(NULL, ".");
-    }
-	
-    char ext[100];
-    strcpy(ext,checkext[n-1]);
-    for(int i = 0; ext[i]; i++){
-       ext[i] = tolower(ext[i]);
-    }
-    
-    DIR *folder, *folderopen;
-    struct dirent *entry;
-    char tujuanfile[100];
-    folder = opendir(cwd);
-    int available = 0;
-
-    if( n > 1 ){
-        if(folder == NULL)printf("error\n");
-        while( (entry=readdir(folder)) ){
-            if(strcmp(entry->d_name,ext) == 0 && entry->d_type == 4){
-                available = 1;
-                break;
-            }
-        }
-
-    if(available == 0){
-            strcpy(tujuanfile,cwd);
-            strcat(tujuanfile,"/");
-            strcat(tujuanfile,ext);
-            mkdir(tujuanfile, 0777);
-        }
-    }
-}
+	char folderpath[1000];
+	strcpy(folderpath, cwd);
+	strcat(folderpath, "/");
+	strcat(folderpath, ext);
+	mkdir(folderpath, S_IRWXU);
+	if(strlen(deepfolder2)>1){
+		char fullname[1000];
+		strcpy(fullname, deepfolder2);
+		strcat(fullname, "/");
+		strcat(fullname, filename);
+		strcat(folderpath, "/");
+        strcat(folderpath, filename);
+        rename(fullname, folderpath);
+	}
+	else if(strlen(folder) > 1) {
+		char fullname[1000];
+		strcpy(fullname, folder);
+		strcat(fullname, "/");
+		strcat(fullname, filename);
+		strcat(folderpath, "/");
+        strcat(folderpath, filename);
+        rename(fullname, folderpath);
+	}
+	else {					
+	    strcat(folderpath, "/");
+        strcat(folderpath, filename);
+		rename(filepath, folderpath);
+	}
 	return NULL;
 }
 
-int main(int argc, char *argv[]) {
-    int err;
-    pthread_t tid[1000];
+void minf(int argc, char *argv[]){
+    for(int i=2; i<argc; i++) {
+		err = pthread_create(&tid[i], NULL, move, (void *)argv[i]);
+		if(err != 0) printf("File %d : Sad, gagal :(\n", i-1);
+        else printf("File %d : Berhasil Dikategorikan\n", i-1);
+	}
+	for(int j=2; j<argc; j++) pthread_join(tid[j], NULL);
+    return;
+}
 
-    if (strcmp(argv[1],"-f") == 0) {
-        for(int j = 2 ; j < argc ; j++ ){
-            err = pthread_create(&tid[j],NULL,movinfile,(void *)argv[j]);
-            
-            if(err != 0){
-                printf("Can't create thread! : [%s]\n",strerror(err));
-            }
-        }
-
-        for(int j = 2; j < argc; j++){
-            pthread_join(tid[j], NULL);
-        }
+void deep(){
+	DIR *dir;
+	struct dirent *tmp;
+	int i=0;
+	memset(deepfolder2, '\0', sizeof(deepfolder2));
+	strcpy(deepfolder2,deepfolder);
+	printf("%s\n", deepfolder2);
+	dir = opendir(deepfolder2);
+	while((dir!=NULL) && (tmp=readdir(dir))) {
+		if(tmp->d_type==DT_DIR && strcmp(tmp->d_name, ".")!=0 && strcmp(tmp->d_name, "..")!=0){
+			strcat(deepfolder2,tmp->d_name);
+			strcat(deepfolder2,"/");
+			deep();
+		}
+        if(strcmp(tmp->d_name, ".")==0 || strcmp(tmp->d_name, "..")==0 || strcmp(tmp->d_name, "soal3.c")==0 || strcmp(tmp->d_name, "soal3")==0 || tmp->d_type==DT_DIR) 
+		continue;
+        err = pthread_create(&tid[i], NULL, move, tmp->d_name);
+        if(err != 0) printf("Yah, gagal disimpan :(\n");
+        else printf("Direktori sukses disimpan!\n");
+        i++;
     }
-return 0; 
+    for(int j=0; j<i; j++) pthread_join(tid[j], NULL);
+    closedir(dir);
+    return;
+}
+
+void other(int argc, char *argv[]){
+    DIR *dir;
+	struct dirent *tmp;
+	int i=0;
+	if(strcmp(argv[1], "-d") == 0) {
+    	dir = opendir(argv[2]);
+		strcpy(folder, argv[2]);
+	}
+	else if((argv[1][0]=='*') && (strlen(argv[1])==1)) {
+		dir = opendir(cwd);
+		strcpy(folder, cwd);
+	}
+	else {
+		printf("Butuh Argumen -f atau -d atau \\*\n");
+        return;
+	}
+	while((dir!=NULL) && (tmp=readdir(dir))) {
+		if(tmp->d_type==DT_DIR && strcmp(tmp->d_name, ".")!=0 && strcmp(tmp->d_name, "..")!=0){
+			strcpy(deepfolder,folder);
+			strcat(deepfolder,"/");
+			strcat(deepfolder,tmp->d_name);
+			strcat(deepfolder,"/");
+			deep();
+		}
+        if(strcmp(tmp->d_name, ".")==0 || strcmp(tmp->d_name, "..")==0 || strcmp(tmp->d_name, "soal3.c")==0 || strcmp(tmp->d_name, "soal3")==0 || tmp->d_type==DT_DIR) 
+		continue;
+        err = pthread_create(&tid[i], NULL, move, tmp->d_name);
+        if(err != 0) printf("Yah, gagal disimpan :(\n");
+        else printf("Direktori sukses disimpan!\n");
+        i++;
+    }
+    for(int j=0; j<i; j++) pthread_join(tid[j], NULL);
+    closedir(dir);
+    return;
+}
+
+int main(int argc, char *argv[]) {
+	getcwd(cwd, sizeof(cwd));
+	memset(folder, '\0', sizeof(folder));
+	memset(deepfolder, '\0', sizeof(deepfolder));
+	memset(deepfolder2, '\0', sizeof(deepfolder2));
+	if(argc < 2) {
+		printf("Butuh Argumen -f atau -d atau \\*\n");
+		return 0;
+	}
+	if(strcmp(argv[1], "-f") == 0) {
+		minf(argc,argv);
+	}
+	else {
+		other(argc,argv);
+	}
+	return 0;
 }
